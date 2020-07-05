a2f464657aeafbee448f3b451dda12cd1f41858e
dafer <fernando.cubides@merlinjobs.com>
2020-07-05 15:14:13 -0500

    create user table

---
+++ migrations/2020-07-03-140041_create_users/up.sql
@@ -0,0 +1,8 @@
+-- Your SQL goes here
+CREATE TABLE users
+(
+    id         SERIAL PRIMARY KEY,
+    username   VARCHAR NOT NULL UNIQUE,
+    password   VARCHAR NOT NULL,
+    first_name VARCHAR NOT NULL
+)
