bf2fbd2f3e7857b87ed149f8ac5352bece018054
dafer <fernando.cubides@merlinjobs.com>
2020-07-05 15:15:31 -0500

    schema for diesel

---
+++ src/schema.rs
@@ -0,0 +1,32 @@
+table! {
+    pets (id) {
+        id -> Int4,
+        name -> Varchar,
+        age -> Nullable<Int2>,
+    }
+}
+
+table! {
+    posts (id) {
+        id -> Int4,
+        user_id -> Int4,
+        text -> Varchar,
+        image -> Varchar,
+        date -> Nullable<Date>,
+    }
+}
+
+table! {
+    users (id) {
+        id -> Int4,
+        username -> Varchar,
+        password -> Varchar,
+        first_name -> Varchar,
+    }
+}
+
+allow_tables_to_appear_in_same_query!(
+    pets,
+    posts,
+    users,
+);
