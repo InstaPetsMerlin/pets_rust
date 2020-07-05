b9a25a14c5c6299c6e3a7dc614cb061a3be84c87
dafer <fernando.cubides@merlinjobs.com>
2020-07-05 15:15:46 -0500

    feat: create models

---
+++ src/models.rs
@@ -0,0 +1,66 @@
+use diesel;
+use diesel::pg::PgConnection;
+use diesel::prelude::*;
+use super::schema::users;
+use super::schema::users::dsl::users as all_users;
+
+#[derive(Serialize, Queryable)] 
+pub struct User {
+    pub id: i32,
+    pub username: String,
+    pub password: String,
+    pub first_name: String,
+}
+
+// decode request data
+#[derive(Deserialize)] 
+pub struct UserData {
+    pub username: String,
+}
+// this is to insert users to database
+#[derive(Serialize, Deserialize, Insertable)]
+#[table_name = "users"]
+pub struct  NewUser {
+    pub username: String,
+    pub password: String,
+    pub first_name: String,
+}
+
+impl User {
+
+    pub fn get_all_users(conn: &PgConnection) -> Vec<User> {
+        all_users
+            .order(users::id.desc())
+            .load::<User>(conn)
+            .expect("error!")
+    }
+
+    pub fn insert_user(user: NewUser, conn: &PgConnection) -> bool {
+        diesel::insert_into(users::table)
+            .values(&user)
+            .execute(conn)
+            .is_ok()
+    }
+
+    pub fn get_user_by_username(username: &String, conn: &PgConnection) -> Vec<User> {
+        all_users
+            .filter(users::username.eq(username))
+            .load::<User>(conn)
+            .expect("error!")
+    }
+}
+// #[derive(Serialize, Queryable)] 
+// pub struct Pet {
+//     pub id: i32,
+//     pub name: String,
+//     pub name: i32,
+// }
+
+// #[derive(Serialize, Queryable)] 
+// pub struct Post {
+//     pub id: i32,
+//     pub user_id: i32,
+//     pub text: String,
+//     pub image: String,
+//     pub date: PgTimestamp,
+// }
