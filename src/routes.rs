7b2721418a038b57dc309aa0903fc5eba07dd03a
dafer <fernando.cubides@merlinjobs.com>
2020-07-07 10:15:44 -0500

    feat: add the route to login and add create the token when you singup or login

--- src/routes.rs
+++ src/routes.rs
@@ -1,8 +1,12 @@
-use super::db::Conn as DbConn;
-use rocket_contrib::json::Json;
-use super::models::{User, NewUser};
-use serde_json::Value;
 use rocket::http::RawStr;
+use rocket_contrib::json::Json;
+use serde_json::Value;
+
+use crate::authentication::generate_token;
+use crate::models::LoginInfo;
+
+use super::db::Conn as DbConn;
+use super::models::{NewUser, User};
 
 #[post("/users", format = "application/json")]
 pub fn get_all(conn: DbConn) -> Json<Value> {
@@ -13,11 +17,35 @@ pub fn get_all(conn: DbConn) -> Json<Value> {
     }))
 }
 
-#[post("/user", format = "application/json", data = "<new_user>")]
+#[post("/singup", format = "application/json", data = "<new_user>")]
 pub fn new_user(conn: DbConn, new_user: Json<NewUser>) -> Json<Value> {
-    Json(json!({
-        "status": User::insert_user(new_user.into_inner(), &conn),
+    let user = new_user.into_inner();
+    let status = User::insert_user(&user, &conn);
+    let token = match status {
+        true => generate_token(&user.username, &user.first_name),
+        false => String::from(""),
+    };
+
+    Json(json!(
+    {
+        "status": status,
         "result": User::get_all_users(&conn).first(),
+        "token": token,
+    }))
+}
+
+#[post("/login", format = "application/json", data = "<login_info>")]
+pub fn login(conn: DbConn, login_info: Json<LoginInfo>) -> Json<Value> {
+    let user = User::get_user_by_username(&String::from(login_info.username.as_str()), &conn);
+
+    let token = match user.len() {
+        1 => generate_token(&user.first().unwrap().username, &user.first().unwrap().first_name),
+        _ => String::from(""),
+    };
+    Json(json!(
+    {
+        "result": user.first(),
+        "token": token,
     }))
 }
 
