1782cc711e6b029e088349f24b6842e82a9ab93f
dafer <fernando.cubides@merlinjobs.com>
2020-07-08 15:39:05 -0500

    feat: add header handler with Apikey Struct

---
+++ src/delivery/api_key.rs
@@ -0,0 +1,34 @@
+use rocket::Outcome;
+use rocket::http::Status;
+use rocket::request::{self, Request, FromRequest};
+use crate::
+use_case::authentication::authenticate;
+use jsonwebtoken::errors::Error;
+
+pub struct ApiKey(pub(crate) String);
+
+/// Returns true if `key` is a valid API key string.
+pub fn is_valid(key: &str) -> Result<bool, Error> {
+    authenticate(&String::from(key.split_whitespace().nth(1).unwrap()))
+}
+
+#[derive(Debug)]
+pub enum ApiKeyError {
+    BadCount,
+    Missing,
+    Invalid,
+}
+
+impl<'a, 'r> FromRequest<'a, 'r> for ApiKey {
+    type Error = ApiKeyError;
+
+    fn from_request(request: &'a Request<'r>) -> request::Outcome<Self, Self::Error> {
+        let keys: Vec<_> = request.headers().get("Authorization").collect();
+        match keys.len() {
+            0 => Outcome::Failure((Status::BadRequest, ApiKeyError::Missing)),
+            1 if is_valid(keys[0]).is_ok() => Outcome::Success(ApiKey(keys[0].to_string())),
+            1 => Outcome::Failure((Status::Unauthorized, ApiKeyError::Invalid)),
+            _ => Outcome::Failure((Status::Unauthorized, ApiKeyError::BadCount)),
+        }
+    }
+}
