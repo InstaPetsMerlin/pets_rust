99a5fcb53be4706e82e5e1209571bca51bab417e
dafer <fernando.cubides@merlinjobs.com>
2020-07-14 17:41:12 -0500

    feat: create post repository

--- src/post/repositories/models.rs
+++ src/post/repositories/models.rs
@@ -3,57 +3,54 @@ use diesel::pg::PgConnection;
 use diesel::prelude::*;
 use diesel::result::Error;
 
-use crate::datasource::schema::users;
-use crate::datasource::schema::users::dsl::users as all_users;
+use crate::datasource::schema::posts;
+use crate::datasource::schema::posts::dsl::posts as all_posts;
 
 #[derive(Serialize, Queryable)]
-pub struct User {
+pub struct Post {
     pub id: i32,
-    pub username: String,
-    pub password: String,
-    pub first_name: String,
+    pub user_id: i32,
+    pub text: String,
+    pub image: String,
+    // pub date: NaiveDateTime,
 }
 
 // decode request data
 #[derive(Deserialize)]
-pub struct UserData {
-    pub username: String,
+pub struct PostSearchParam {
+    pub user_id: i32,
 }
 
-// this is to insert users to database
+// this is to insert posts to database
 #[derive(Serialize, Deserialize, Insertable)]
-#[table_name = "users"]
-pub struct NewUser {
-    pub username: String,
-    pub password: String,
-    pub first_name: String,
+#[table_name = "posts"]
+pub struct NewPost {
+    pub user_id: i32,
+    pub text: String,
+    pub image: String,
 }
 
-#[derive(Serialize, Deserialize, Insertable)]
-#[table_name = "users"]
-pub struct LoginInfo {
-    pub username: String,
-    pub password: String,
-}
-
-impl User {
-    pub fn get_all_users(conn: &PgConnection) -> Vec<User> {
-        all_users
-            .order(users::id.desc())
-            .load::<User>(conn)
+impl Post {
+    pub fn get_all_posts(conn: &PgConnection) -> Vec<Post> {
+        all_posts
+            .order(posts::id.desc())
+            .load::<Post>(conn)
             .expect("error!")
     }
 
-    pub fn insert_user(user: &NewUser, conn: &PgConnection) -> bool {
-        diesel::insert_into(users::table)
-            .values(user)
+    pub fn insert_post(post: &NewPost, conn: &PgConnection) -> bool {
+        diesel::insert_into(posts::table)
+            .values(post)
             .execute(conn)
             .is_ok()
     }
 
-    pub fn get_user_by_username(username: &String, conn: &PgConnection) -> Result<Vec<User>, Error> {
-        return match all_users.filter(users::username.eq(username)).load::<User>(conn) {
-            Ok(users) => Ok(users),
+    pub fn get_posts_by_user_id(user_id: i32, conn: &PgConnection) -> Result<Vec<Post>, Error> {
+        return match all_posts
+            .filter(posts::user_id.eq(user_id))
+            .load::<Post>(conn)
+        {
+            Ok(posts) => Ok(posts),
             Err(e) => Err(e),
         };
     }
