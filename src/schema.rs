919bad09923fe09c1a9ec877b817076c2bc9dcec
dafer <fernando.cubides@merlinjobs.com>
2020-07-14 17:41:34 -0500

    feat: update schema

---
+++ src/schema.rs
@@ -0,0 +1,31 @@
+table! {
+    pets (id) {
+        id -> Int4,
+        name -> Varchar,
+        age -> Nullable<Int2>,
+    }
+}
+
+table! {
+    posts (id) {
+        id -> Int4,
+        user_id -> Int4,
+        text -> Varchar,
+        image -> Varchar,
+    }
+}
+
+table! {
+    users (id) {
+        id -> Int4,
+        username -> Varchar,
+        password -> Varchar,
+        first_name -> Varchar,
+    }
+}
+
+allow_tables_to_appear_in_same_query!(
+    pets,
+    posts,
+    users,
+);
