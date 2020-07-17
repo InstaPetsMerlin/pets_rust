57e12701fa2f8d4c8e198556ffa171e03635856f
dafer <fernando.cubides@merlinjobs.com>
2020-07-17 10:08:27 -0500

    feat: update signature of profile repository trait

--- src/profile/repositories/profile.rs
+++ src/profile/repositories/profile.rs
@@ -6,5 +6,5 @@ use std::error::Error;
 pub trait ProfileRepository {
     fn get_all_users(&self) -> Result<Vec<User>, ProfileError>;
     fn get_user_by_username(&self, username: String) -> Result<User, ProfileError>;
-    fn insert_user(&self, username: String) -> Result<User, ProfileError>;
+    fn insert_user(&self, user: User) -> Result<User, ProfileError>;
 }
