43549b7fcabc84e52f1f3047035fda3b803f948e
dafer <fernando.cubides@merlinjobs.com>
2020-07-17 10:08:55 -0500

    feat: add create user to profile use case

--- src/profile/use_case/profile.rs
+++ src/profile/use_case/profile.rs
@@ -5,7 +5,7 @@ use crate::profile::domain::User;
 use crate::profile::errors::ProfileError;
 
 pub trait ProfileManager {
-    // fn create_user(&self,user: User) -> User;
+    fn create_user(&self,user: User) -> Result<User, ProfileError>;
     // fn update_user(&self,user: User) -> User;
     // fn delete_user(&self,user: User) -> User;
     fn get_user_by_username(&self, username: String) -> Result<User, ProfileError>;
