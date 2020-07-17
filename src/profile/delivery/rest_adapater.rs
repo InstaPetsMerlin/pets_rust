3dea1cf9aa4388683da263d8e255a3bbe7ce3984
dafer <fernando.cubides@merlinjobs.com>
2020-07-17 10:47:44 -0500

    feat:rename rest adapter and update mod

--- src/profile/delivery/rest_adapater.rs
+++ src/profile/delivery/rest_adapater.rs
@@ -9,6 +9,10 @@ use crate::datasource::schema::users::columns::username;
 use crate::profile::delivery::api_key::{is_valid, ApiKey};
 use crate::profile::domain::User;
 use crate::profile::use_case::profile::ProfileManager;
+use crate::profile::repositories::models::NewUser;
+use crate::profile::errors::ProfileError;
+use crate::profile::delivery::sign_up_response::SignUpResponse;
+use crate::profile::use_case::authentication::generate_token;
 
 pub struct ProfileRestAdapter {
     profile_manager: Box<dyn ProfileManager>,
@@ -18,6 +22,17 @@ impl ProfileRestAdapter {
     pub fn new(profile_manager: Box<dyn ProfileManager>) -> Self {
         Self { profile_manager }
     }
+
+    pub fn sing_up(&self, new_user: Json<NewUser>) -> Result<Json<Value>, ProfileError>{
+        match self.profile_manager.create_user(User{ username: new_user.into_inner().username }) {
+            Ok(user) => {
+                let token = generate_token(&user.username, &user.username);
+                let response = SignUpResponse { token, user: PresentationUser{ username: user.username } };
+                Ok(Json(json!(response)))
+            },
+            Err(_) => Err(ProfileError::Other("Could not create user".to_string())),
+        }
+    }
     pub fn get_user(&self, user_name: &RawStr, key: ApiKey) -> Result<Json<Value>, Box<dyn Error>> {
         match is_valid(&*key.0) {
             Ok(_) => Ok(self.find_user(user_name.to_string())),
@@ -25,15 +40,15 @@ impl ProfileRestAdapter {
         }
     }
 
-    pub fn get_all_user(&self, conn: Conn, key: ApiKey) -> Result<Json<Value>, Box<dyn Error>> {
+    pub fn get_all_user(&self,key: ApiKey) -> Result<Json<Value>, ProfileError> {
         return match is_valid(&*key.0) {
-            Ok(_) => Ok(self.find_users(conn)),
-            Err(e) => Err(e.into()),
+            Ok(_) => {self.find_users()},
+            Err(e) => Err(ProfileError::Other("Not Authorized".to_string())),
         };
     }
 
-    fn find_users(&self, conn: Conn) -> Json<Value> {
-        match self.profile_manager.get_all_users(conn) {
+    fn find_users(&self) -> Result<Json<Value>,ProfileError >{
+        match self.profile_manager.get_all_users() {
             Ok(users) => {
                 let result: Vec<PresentationUser> = users
                     .into_iter()
@@ -41,9 +56,9 @@ impl ProfileRestAdapter {
                         username: u.username,
                     })
                     .collect();
-                Json(json!({ "result": result }))
+                Ok(Json(json!({ "users": result , "total" : result.len()})))
             }
-            Err(e) => Json(json!({ "result": Vec::<String>::new() })),
+            Err(e) => Err(ProfileError::ProfileNotFoundError("User not found".to_string())),
         }
     }
 
