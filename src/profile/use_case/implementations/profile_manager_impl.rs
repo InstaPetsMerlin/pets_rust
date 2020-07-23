397249d87af3010e81877e0cab3ad32eef3167e6
dafer <fernando.cubides@merlinjobs.com>
2020-07-23 09:45:37 -0500

    feat: remove DB connection from manager

--- src/profile/use_case/implementations/profile_manager_impl.rs
+++ src/profile/use_case/implementations/profile_manager_impl.rs
@@ -19,12 +19,12 @@ impl<T: ProfileRepository> ProfileManagerImpl<T> {
 }
 
 impl<T: ProfileRepository> ProfileManager for ProfileManagerImpl<T> {
-    fn create_user(&self, user: User, conn: Conn) -> Result<User, ProfileError> {
-        self.profile_repo.insert_user(user, conn)
+    fn create_user(&self, user: User) -> Result<User, ProfileError> {
+        self.profile_repo.insert_user(user)
     }
 
-    fn update_user(&self, user: User, conn: Conn) -> Result<User, ProfileError>  {
-        self.profile_repo.update_user(user,conn)
+    fn update_user(&self, user: User) -> Result<User, ProfileError>  {
+        self.profile_repo.update_user(user)
     }
 
     //
@@ -32,15 +32,15 @@ impl<T: ProfileRepository> ProfileManager for ProfileManagerImpl<T> {
     //     unimplemented!()
     // }
     //
-    fn delete_user(&self, user_id: String, conn: Conn) -> Result<User, ProfileError> {
-        self.profile_repo.delete_user(user_id,conn)
+    fn delete_user(&self, user_id: String) -> Result<User, ProfileError> {
+        self.profile_repo.delete_user(user_id)
     }
 
-    fn get_user_by_username(&self, username: String, conn: Conn) -> Result<User, ProfileError> {
-        self.profile_repo.get_user_by_username(username, conn)
+    fn get_user_by_username(&self, username: String) -> Result<User, ProfileError> {
+        self.profile_repo.get_user_by_username(username)
     }
 
-    fn get_all_users(&self, conn: Conn) -> Result<Vec<User>, ProfileError> {
-        self.profile_repo.get_all_users(conn)
+    fn get_all_users(&self) -> Result<Vec<User>, ProfileError> {
+        self.profile_repo.get_all_users()
     }
 }
