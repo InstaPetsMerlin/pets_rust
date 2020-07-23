3d1ef69cde1117a2879879400fc868e2f9c1f687
dafer <fernando.cubides@merlinjobs.com>
2020-07-23 09:43:31 -0500

    feat: create response object for manage body and status in the response

---
+++ src/profile/delivery/response.rs
@@ -0,0 +1,41 @@
+use rocket::{Response, response};
+use rocket::http::{ContentType, Status};
+use rocket::Request;
+use rocket::response::Responder;
+use rocket_contrib::json::{Json, JsonValue};
+use serde_json::Value;
+
+#[derive(Debug)]
+pub struct HttpResponse {
+    pub body: JsonValue,
+    pub status: Status,
+}
+
+impl<'a> Responder<'a> for HttpResponse {
+    fn respond_to(self, request: &Request) -> response::Result<'a> {
+        Response::build_from(self.body.respond_to(&request).expect("could not generate response"))
+            .header(ContentType::JSON)
+            .ok()
+    }
+}
+
+#[derive(Debug)]
+pub struct ResponseError {
+    pub body: JsonValue,
+    pub status: Status,
+}
+
+
+#[derive(Debug)]
+pub struct HttpResponseError {
+    pub body: JsonValue,
+    pub status: Status,
+}
+
+impl<'a> Responder<'a> for HttpResponseError {
+    fn respond_to(self, request: &Request) -> response::Result<'a> {
+        Response::build_from(self.body.respond_to(&request).expect("could not generate response"))
+            .header(ContentType::JSON)
+            .ok()
+    }
+}
